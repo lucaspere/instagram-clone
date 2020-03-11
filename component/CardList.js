5f9be7ac98492483be83c615966f33d4ae24296b
lucaspere <lucasrupestre@hotmail.com>
2020-03-10 21:17:15 -0300

    refatorando o componente CardList

--- component/CardList.js
+++ component/CardList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { getImageFromId } from '../utils/api';
@@ -7,24 +7,33 @@ import { getImageFromId } from '../utils/api';
 import Card from './Card';
 
 
-const CardList = ({items}) => {
+const CardList = ({ items, commentsForItem, onPressComments }) => {
 
-   const keyExtractor = ({id}) => id.toString();
+   const keyExtractor = ({ id }) => id.toString();
 
-   const renderitem = ({item: {id, author}}) => (
-      <Card
-         fullname={author}
-         image={{
-            uri: getImageFromId(id)
-         }}
-      />
-   );
+   const renderitem = ({ item: { id, author } }) => {
+
+   const comment = commentsForItem[id]
+
+   return (
+         <Card
+            fullname={author}
+            image={{
+               uri: getImageFromId(id)
+            }}
+            linkText={`${comment ? comment.length : 0} Comentários`}
+            onPressLinkText={() => onPressComments(id)}
+            id={id}
+         />
+      )
+   };
 
    return (
       <FlatList
          data={items}
          renderItem={renderitem}
          keyExtractor={keyExtractor}
+         extraData={commentsForItem}
       />
    )
 }
@@ -35,7 +44,10 @@ CardList.propTypes = {
          id: PropTypes.number.isRequired,
          author: PropTypes.string.isRequired
       })
-   ).isRequired
+   ).isRequired,
+   commentsForItem: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string))
+      .isRequired,
+   onPressComments: PropTypes.func.isRequired
 };
 
 export default CardList;
