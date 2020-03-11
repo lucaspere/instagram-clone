7054ac8727ab0846dc46bdd0d1b551540ff6a9ce
lucaspere <lucasrupestre@hotmail.com>
2020-03-10 21:19:51 -0300

    consertando o componente top-level para ficar sincronizado com os outros

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, View, Platform, Modal, platformVersion } from 'react-native';
 import Constants from 'expo-constants';
 
 
 import Feed from './screens/Feed';
-import CommentInput from './component/CommentInput';
-import CommentList from './component/CommentList';
-import NavigationBar from './component/NavigationBar';
+import Comments from './screens/Comments';
 
 const App = () => {
 
+  const [commentsForItem, setCommentsForItem] = React.useState({});
+  const [showModal, setShowModal] = React.useState(false);
+  const [selectedItemId, setSelectItemId] = React.useState(null);
+
   return (
     <View style={styles.container}>
-      <NavigationBar title="Lucas" leftText="Queridinho Lucas" />
-      <CommentInput />
-      <CommentList items={[..."rffe".repeat(3)]}/>
+      <Feed
+        style={styles.feed}
+        onPressComments={id => {
+          setShowModal(true);
+          setSelectItemId(id)
+        }}
+        commentsForItem={commentsForItem}
+      />
+      <Modal
+        visible={showModal}
+        animationType="slide"
+        onRequestClose={() => {
+          setShowModal(false);
+          setSelectItemId(null)
+        }}
+      >
+        <Comments
+          style={styles.container}
+          comments={commentsForItem[selectedItemId] || []}
+          onClose={() => {
+            setShowModal(false);
+            setSelectItemId(null)
+          }}
+          onSubmitComment={text => {
+            if (!commentsForItem.hasOwnProperty(selectedItemId)) {
+              const newComment = {
+                [selectedItemId]: [text]
+              }
+              return setCommentsForItem(newComment)
+            }
+
+            const newComment = {
+              [selectedItemId]: [text, ...commentsForItem[selectedItemId]]
+            }
+
+            setCommentsForItem(newComment)
+          }}
+        />
+      </Modal>
     </View>
   );
 }
@@ -26,7 +64,14 @@ const styles = StyleSheet.create({
   },
   feed: {
     flex: 1,
-    marginTop: 
+    marginTop:
+      Platform.OS === 'ios' && platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0
+  },
+  comments: {
+    flex: 1,
+    marginTop:
       Platform.OS === 'android' || platformVersion < 11
         ? Constants.statusBarHeight
         : 0
