4d54a0ca6e9d3193226c79611a46cdf8468f4522
lucaspere <lucasrupestre@hotmail.com>
2020-03-10 21:18:16 -0300

    mundando a função fetchImage para usar async/await

--- utils/api.js
+++ utils/api.js
@@ -1,19 +1,27 @@
-export const fetchImages = async (callback) => {
+export const fetchImages = async (setItems, setLoading, setError, signal) => {
 
    try {
 
-      const response = await fetch('https://unsplash.it/list');
+      const response = await fetch('https://unsplash.it/list', { signal });
 
       const images = await response.json()
 
-      callback(undefined, images)
-      
-   } catch(erro) {
-      
-      callback(erro, undefined)
-      
+      if (images.length === 0) {
+
+         setError({
+            isError: true,
+            errorMessage: 'Falha ao carregar a imagem'
+         })
+      }
+
+      setItems(images);
+      setLoading(false);
+
+   } catch (erro) {
+
    }
 }
+
 
 export const getImageFromId = id =>
    `https://unsplash.it/${600}/${600}?image=${id}`;
